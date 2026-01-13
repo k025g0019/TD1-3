30113a9b05ab75d4296cc1ffcaf2baf83727db84
LC1B_29_mano <k025g0019@g.neec.ac.jp>
2026-01-13 16:12:49 +0900

    検証

--- TD1-3/camera.h
+++ TD1-3/camera.h
@@ -4,25 +4,25 @@
 
 class Camera {
 public:
-    float x;
-    float y;
+	float x;
+	float y;
+	// ▼ カメラ振動用変数（既存）
+	int shakeTimer;
+	float shakeRange;
 
-    int shakeTimer;
-    float shakeRange;
+	// ★ シングルトン取得
+	static Camera& Instance() {
+		static Camera instance;
+		return instance;
+	}
 
-    // ★ シングルトン取得
-    static Camera& Instance() {
-        static Camera instance;
-        return instance;
-    }
+	// ▼ プレイヤー追従（これを追加！）
+	void Follow(float playerX, float playerY);
 
-    // ▼ プレイヤー追従（これを追加！）
-    void Follow(float playerX, float playerY);
-
-    // ▼ カメラ振動（既存）
-    void StartShake(int duration, float range);
-    void UpdateShake();
+	// ▼ カメラ振動（既存）
+	void StartShake(int duration, float range);
+	void UpdateShake();
 
 private:
-    Camera() : x(0), y(0), shakeTimer(0), shakeRange(0) {}
+	Camera() : x(0), y(0), shakeTimer(0), shakeRange(0) {}
 };
